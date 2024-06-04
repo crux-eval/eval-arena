5aebe34678ed235c2fc39e07fb5941447aceca35
Sida Wang <sida@fb.com>
2024-06-04 04:11:36 +0000

    clean up

--- report_example.py
+++ report_example.py
@@ -1,18 +1,10 @@
-from collections import defaultdict
-import json, math, glob
 import numpy as np
 import pandas as pd
-import scipy.stats as stats
 import plotly.express as px
-from tqdm import tqdm
 import math
-import os
-from arena import result_table, pass1_to_battle, example_table
-
-display = print
+from arena import model_table, pass1_to_battle, example_table
 
 def get_anchor(benchmark_id: str, example_id: str):
-    # supporting {'humaneval+', 'CRUXEval-input', 'mbpp+', 'CRUXEval-output'}
     def get_link():
         if benchmark_id in ['humaneval', 'humaneval+', 'mbpp', 'mbpp+']:
             dir, id = example_id.split('/') # expecting HumanEval/93 and Mbpp/622 etc.
@@ -28,11 +20,11 @@ def get_anchor(benchmark_id: str, example_id: str):
     else:
         return example_id
 
-def get_example_level_results(benchmark_id):
-    result = eval_results[eval_results['benchmark_id'] == benchmark_id]
+
+def get_example_level_results(benchmark_id, result):
     battles = pass1_to_battle(result)
     battles_no_ties = battles[battles["winner"].str.contains("model_")]
-    all_stats = result_table(battles_no_ties, result)
+    all_stats = model_table(battles_no_ties, result)
     ex_table = example_table(result, all_stats)
     ex_table['example_link'] = ex_table['example_id'].apply(lambda x: get_anchor(benchmark_id, x))
 
@@ -44,41 +36,25 @@ def get_example_level_results(benchmark_id):
     no_solve = ex_table[ex_table['num_solved'] == 0]
     outputs['list_no_solve'] = sorted(no_solve['example_link'].to_list())
     one_solve = ex_table[ex_table['num_solved'] == 1]
-    display(one_solve)
+    pd.options.mode.chained_assignment = None 
     one_solve['model'] = one_solve['models'].apply(lambda x: x[0])
     one_solve = one_solve.sort_values(by='min_elo', ascending=False)
     one_solve = one_solve[['example_link', 'model', 'min_elo']]
-    display(one_solve)
     outputs['table_one_solve'] = one_solve.to_html(escape=False, float_format='%10.3f', index=False)
 
-    elo75 = all_stats['elo'].quantile(0.75)
-    print(elo75)
     list_suspect = ex_table.sort_values(by='tau', ascending=True).head(10)
     outputs['table_suspect'] = list_suspect[['example_link', 'acc', 'tau']].to_html(escape=False, float_format='%10.3f', index=False)
     print(benchmark_id, 'anti-correlated prop', np.mean(ex_table['tau'] <= 0))
-    print(ex_table['tau'].describe())
 
-    print(outputs.keys())
     return outputs
 
-records = []
-for fname in glob.glob(f"data/*.jsonl"):
-    with open(fname, 'rt') as f:
-        records.extend([json.loads(l) for l in f.readlines()])
 
-eval_results = pd.DataFrame(records)
-print(eval_results.describe())
-
-def gen_report(benchmark_id: str):
-    outputs = get_example_level_results(benchmark_id)
+def gen_example_report(benchmark_id: str, raw_results: pd.DataFrame, OUTPUT_PATH):
+    outputs = get_example_level_results(benchmark_id, raw_results)
     from jinja2 import Template
-    template_path = r"examplelevel_template.html"
-    output_path = rf"crux-eval.github.io/eval-arena/ex_{benchmark_id}.html"
+    template_path = r"templates/template_example.html"
+    output_path = rf"{OUTPUT_PATH}/ex_{benchmark_id}.html"
     with open(output_path, "w", encoding="utf-8") as output_file:
         with open(template_path) as template_file:
             j2_template = Template(template_file.read())
             output_file.write(j2_template.render({'benchmark_id': benchmark_id, 'outputs': outputs}))
-
-for b in set(eval_results['benchmark_id']):
-    gen_report(b)
-    # outputs['fig_unique_solves'] = px.histogram(one_solve, x='model').update_xaxes(categoryorder='total descending')
--- report_model.py
+++ report_model.py
@@ -1,6 +1,3 @@
-import json, math, glob
-from collections import Counter
-
 import numpy as np
 import pandas as pd
 import scipy.stats as stats
@@ -8,7 +5,7 @@ import scipy.stats as stats
 import plotly.express as px
 import plotly.graph_objects as go
 
-from arena import result_table, pass1_to_battle, battle_summary
+from arena import model_table, pass1_to_battle, battle_summary
 
 def fig_diff_vs_sum(bmname: str, diffvsum: pd.DataFrame):
     figs = px.scatter(diffvsum, x=diffvsum['diff'].abs(), y='sum',
@@ -45,6 +42,7 @@ def fig_diff_vs_sum(bmname: str, diffvsum: pd.DataFrame):
     )
     return fig
 
+
 def fig_accs_and_pvalues(bmname, diffvsum):
     figs = px.scatter(diffvsum, x='accA', y='accB',
             color='pvalue', range_color=[0, 0.2],
@@ -59,7 +57,6 @@ def fig_accs_and_pvalues(bmname, diffvsum):
         "std(acc(A)-acc(B)): %{customdata[5]:.4f}", 
         ])  + '<extra></extra>')
     
-    # fig = go.Figure(data=figs.data)
     figs.update_layout(
         width=800, height=800,
         title=bmname,
@@ -69,6 +66,7 @@ def fig_accs_and_pvalues(bmname, diffvsum):
     )
     return figs
 
+
 def get_sections(result: pd.DataFrame, benchmark_id):
     battles = pass1_to_battle(result)
     battles_no_ties = battles[battles["winner"].str.contains("model_")]
@@ -76,38 +74,24 @@ def get_sections(result: pd.DataFrame, benchmark_id):
     sections = {
         "fig_accs_and_pvalues": fig_accs_and_pvalues(benchmark_id, summary).to_html(full_html=False),
         "fig_diff_vs_sum": fig_diff_vs_sum(benchmark_id, summary).to_html(full_html=False),
-        "result_table (no ties)": result_table(battles_no_ties, result).to_html(
+        "model_table": model_table(battles_no_ties, result).to_html(
             index=False,
             formatters={
                 'pass1': '{:.1%}'.format,
                 'win_rate': '{:.1%}'.format,
                 'elo': '{:.1f}'.format
         }),
-        # "result_table": result_table(battles, result).style \
-        #     .format(precision=3).to_html()
     }
     return sections
 
 
-def gen_benchmark_report(benchmark_id: str, benchmark_results, output_path):
+def gen_model_report(benchmark_id: str, benchmark_results, OUTPUT_PATH):
     sections = get_sections(benchmark_results, benchmark_id)
     from jinja2 import Template
-    template_path=r"agg_template_description.html"
-    output_path = f"{output_path}/agg_{benchmark_id}.html"
+    template_path=r"templates/template_model.html"
+    output_path = f"{OUTPUT_PATH}/model_{benchmark_id}.html"
     with open(output_path, "w", encoding="utf-8") as output_file:
         with open(template_path) as template_file:
             j2_template = Template(template_file.read())
             output_file.write(j2_template.render({'benchmark_id': benchmark_id, 'sections': sections}))
 
-OUTPUT_PATH = 'crux-eval.github.io/eval-arena'
-if __name__ == '__main__':
-    records = []
-    for fname in glob.glob(f"data/*.jsonl"):
-        with open(fname, 'rt') as f:
-            records.extend([json.loads(l) for l in f.readlines()])
-    eval_results = pd.DataFrame(records)
-    benchmarks = set(eval_results['benchmark_id'])
-    for b in benchmarks:
-        benchmark_result = eval_results[eval_results['benchmark_id'] == b]
-        gen_benchmark_report(b, benchmark_result, OUTPUT_PATH)
-# pushd .; cd crux-eval.github.io/; git commit -am 'report'; git push; popd
