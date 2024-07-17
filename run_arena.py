7f92b6e0a1efb5a77243c3eb62ba27cfb9f9587d
sida <sida@meta.com>
2024-07-17 22:52:17 +0000

    add data

--- run_arena.py
+++ run_arena.py
@@ -70,7 +70,7 @@ def generate_summary(eval_results: pd.DataFrame, OUTPUT_PATH):
             }))
 
 records = []
-for fname in glob.glob(f"raw-data/*_hf.jsonl") + glob.glob("data/*.jsonl"):
+for fname in glob.glob("data/*.jsonl"):
     with open(fname, 'rt') as f:
         records.extend([json.loads(l) for l in f.readlines()])
 eval_results = pd.DataFrame(records)
